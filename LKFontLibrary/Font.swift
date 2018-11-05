d24d74e68e6a9c342cc392df6c3a7b837713531e
penumutchu.prasad@gmail.com <penumutchu.prasadgmail.com@penumutchu-MacBook-Pro.local>
2018-11-05 13:26:29 +0530

    A wrapper to handle fonts in iOS

---
+++ LKFontLibrary/Font.swift
@@ -0,0 +1,99 @@
+//Font.swift
+/*
+ * LKFontLibrary
+ * Created by penumutchu.prasad@gmail.com on 02/11/18
+ * Is a product created by abnboys
+ * For the LKFontLibrary in the LKFontLibrary
+ 
+ * Here the permission is granted to this file with free of use anywhere in the IOS Projects.
+ * Copyright © 2018 ABNBoys.com All rights reserved.
+*/
+
+import UIKit
+
+struct Font {
+    
+    enum FontType {
+        
+        case display
+        case text
+        case custom(String)
+        
+        func value() -> String {
+            var newFont = ""
+            
+            switch self {
+            case .display:
+                newFont = "SFProDisplay-"
+            case .text:
+                newFont = "SFProText-"
+            case .custom(let fontName):
+                newFont = "\(fontName)-"
+            }
+            
+            return newFont
+        }
+        
+    }
+    
+    enum TraitType: String {
+        
+        case Regular
+        case Medium
+        case Bold
+        case Semibold
+        
+        func trait() -> String {
+            return rawValue
+        }
+        
+    }
+    
+    enum FontSize {
+        
+        case standard
+        case heading
+        case custom(CGFloat)
+        
+        var value: CGFloat {
+            switch self {
+            case .standard:
+                return 16.0
+            case .heading:
+                return 28.0
+            case .custom(let customSize):
+                return customSize
+            }
+        }
+        
+    }
+    
+    static func getFont(with type: FontType, withTrait trait: TraitType, ofSize size: FontSize = .standard) -> UIFont {
+        
+        var fontName: String
+        
+        switch type {
+        case .text:
+            fontName = "\(type.value())\(trait.rawValue)"
+        case .display:
+            fontName = "\(type.value())\(trait.rawValue)"
+        default:
+            fontName = "\(type.value())\(trait.rawValue)"
+        }
+        
+        let arra = fontName.components(separatedBy: "-")
+        let (fnt,style) = (arra.first!, arra.last!)
+        if UIFont.familyNames.contains(fnt) {
+            if !((UIFont.fontNames(forFamilyName: fnt)).contains(style)) {
+                
+                return UIFont.init(name: fnt, size: size.value)!
+            }
+        }
+        
+        return UIFont.init(name: fontName, size: size.value)!
+        
+    }
+    
+}
+
+
--- LKFontLibrary/ViewController.swift
+++ LKFontLibrary/ViewController.swift
@@ -12,13 +12,61 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    lazy var lbl1 = createLabel()
+    lazy var lbl2 = createLabel()
+    lazy var lbl3 = createLabel()
+    lazy var lbl4 = createLabel()
+    lazy var lbl5 = createLabel()
+    @IBOutlet var nextButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let stack = UIStackView(arrangedSubviews: [lbl1, lbl2, lbl3, lbl4, lbl5])
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.axis = .vertical
+        stack.spacing = 8
+        stack.distribution = .fillEqually
+        view.addSubview(stack)
+        
+        view.addConstraints([
+            
+            stack.topAnchor.constraint(equalTo: view.topAnchor, constant: 24),
+            stack.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            stack.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            stack.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.7)
+            
+            ])
+        
+        setFonts()
+
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+       
+    }
+    
+    private func setFonts() {
+        
+        lbl1.font = UIFont.init(name: "Helvetica", size: 14)
+        lbl2.font = Font.getFont(with: .custom("Helvetica"), withTrait: .Bold, ofSize: .custom(18))
+        lbl3.font = Font.getFont(with: .display, withTrait: .Bold, ofSize: .custom(22))
+        lbl4.font = Font.getFont(with: .text, withTrait: .Semibold, ofSize: .standard)
+        lbl5.font = Font.getFont(with: .custom("Papyrus"), withTrait: .Regular, ofSize: .heading)
+        
+        nextButton.titleLabel?.font = Font.getFont(with: .custom("Papyrus"), withTrait: .Regular, ofSize: Font.FontSize.custom(18))
+        
     }
 
-    override func didReceiveMemoryWarning() {
+  
+    func createLabel() -> UILabel {
+        
+        let lbl  = UILabel()
+        lbl.text = "welcome to swift programming"
+        return lbl
     }
 
 
